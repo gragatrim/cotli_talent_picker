271b688be534c97a1fe3bab28e54dc5d84e17bce
Grag Atrim <codingame@gragatrim.com>
2021-05-05 07:28:55 -0400

    Make runes thier own column
    Thanks to Purple for suggesting it

--- fifth_runes.php
+++ fifth_runes.php
@@ -1,22 +1,23 @@
 <?php
   include "navigation.php";
   $game_defines = new GameDefines();
-  echo '<table style="margin-left: auto; margin-right: auto"><tr><th colspan="16">Crusader 5th Rune Scaling(all numbers are percentages)</th></tr><tr><th>Crusader Name</th>
-  <th> Lvl 1</th>
-  <th> Lvl 2</th>
-  <th> Lvl 3</th>
-  <th> Lvl 4</th>
-  <th> Lvl 5</th>
-  <th> Lvl 6</th>
-  <th> Lvl 7</th>
-  <th> Lvl 8</th>
-  <th> Lvl 9</th>
-  <th> Lvl 10</th>
-  <th> Lvl 11</th>
-  <th> Lvl 12</th>
-  <th> Lvl 13</th>
-  <th> Lvl 14</th>
-  <th> Lvl 15</th>
+  echo '<table style="margin-left: auto; margin-right: auto"><tr><th colspan="17">Crusader 5th Rune Scaling(all numbers are percentages)</th></tr><tr><th>Crusader Name</th>
+  <th>Rune</th>
+  <th>Lvl 1</th>
+  <th>Lvl 2</th>
+  <th>Lvl 3</th>
+  <th>Lvl 4</th>
+  <th>Lvl 5</th>
+  <th>Lvl 6</th>
+  <th>Lvl 7</th>
+  <th>Lvl 8</th>
+  <th>Lvl 9</th>
+  <th>Lvl 10</th>
+  <th>Lvl 11</th>
+  <th>Lvl 12</th>
+  <th>Lvl 13</th>
+  <th>Lvl 14</th>
+  <th>Lvl 15</th>
   </tr>';
   $row_number = 0;
   foreach($game_defines->crusaders AS $crusader) {
@@ -37,7 +38,7 @@
       foreach ($crusader->hero_gem_slots[5]->effects AS $rune_effect) {
         $fifth_slot_effect = get_gem_effect($rune_effect, $game_defines);
         $fifth_slot_effect = trim(str_replace('<br>', ', ', $fifth_slot_effect), ', ');
-        $rune_levels .= '<tr ' . $row_style . '><td><b>' . $crusader->name . '(' . $fifth_slot_effect . ') ' . $rune_type . ' Rune</b></td>';
+        $rune_levels .= '<tr ' . $row_style . '><td><b>' . $crusader->name . '(' . $fifth_slot_effect . ')</b></td><td><b>' . $rune_type . '</b></td>';
         if (is_array($rune_effect->level_amounts)) {
           $i = 1;
           foreach ($rune_effect->level_amounts AS $level => $amount) {
