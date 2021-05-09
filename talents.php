b9ce81df89b340dc87e01f2cd91019e700ee5f5a
Grag Atrim <codingame@gragatrim.com>
2021-05-09 10:43:28 -0400

    Implement Power Saws Ablaze in talent calc
    Now will properly suggest buying power saws ablaze, always assumes
    you'll have it active.

--- talents.php
+++ talents.php
@@ -1,8 +1,8 @@
 <?php
 
 //Semi global, mainly I'm just lazy, if you add to these arrays, you must add it to the dps_talents array in the talent constructor
-$fully_implemented_talents = array(119, 3, 66, 63, 118, 92, 120, 5, 16, 17, 23, 31, 54, 121, 34, 36, 39, 41, 56, 122, 74, 104, 106, 107, 114, 115, 105, 111, 112, 113, 73, 123, 126);
-$partially_implemented_talents = array(55, 102 );
+$fully_implemented_talents = array(119, 3, 66, 63, 118, 92, 120, 5, 16, 17, 23, 31, 54, 121, 34, 36, 39, 41, 56, 122, 74, 104, 106, 107, 114, 115, 105, 111, 112, 113, 73, 123, 125, 126);
+$partially_implemented_talents = array(55, 102);
 class Talent {
   function __construct($id, $name, $tier, $max_level, $base_cost, $level_multiplier, $current_level = 0, $damage_type = '', $damage_base = 0, $damage_base_multiplier = 1, $stacks = 1, $main_dps_slot = 0, $level_costs = array()) {
     $this->id = $id;
@@ -37,7 +37,7 @@ class Talent {
                         17 => '2107159763941',
                         18 => '3769708817690'];
     $this->level_costs = $level_costs;
-    $this->dps_talents = array(119, 3, 66, 63, 118, 92, 120, 5, 16, 17, 23, 31, 54, 121, 34, 36, 39, 41, 56, 122, 74, 104, 106, 107, 114, 115, 105, 111, 112, 113, 73, 55, 102, 123, 126);
+    $this->dps_talents = array(119, 3, 66, 63, 118, 92, 120, 5, 16, 17, 23, 31, 54, 121, 34, 36, 39, 41, 56, 122, 74, 104, 106, 107, 114, 115, 105, 111, 112, 113, 73, 55, 102, 123, 125, 126);
   }
 
   public function get_damage_at_additional_level($levels_to_add) {
@@ -89,7 +89,8 @@ class Talent {
       $this->stacks = $this->current_level;
     } else if ($this->name == 'front_line_fire'
             || $this->name == 'backline_defensive'
-            || $this->name == 'must_be_magic') {
+            || $this->name == 'must_be_magic'
+            || $this->name == 'power_saws_ablaze') {
       $this->stacks = ($this->current_level - 1);
     } else if ($this->name == 'dressing_for_success') {
         $this->damage_base = (2 * $this->current_level);
@@ -121,7 +122,7 @@ class Talent {
       $damage = bcmul(bcpow(bcmul($this->damage_base, $this->damage_base_multiplier, 40), $this->stacks, 40), 100, 40);
     } else if ($this->name == 'dressing_for_success' || $this->name == 'trinket_hoarder') {
       $damage = bcmul(bcmul(bcadd('1', bcdiv(($this->damage_base + 2), '100', 20)), bcpow(bcadd(bcdiv($this->damage_base_multiplier, 100, 20), 1, 20), ($this->stacks - 1), 20)), 100, 20);
-    } else if ($this->name == 'front_line_fire' || $this->name == 'formation_full_up' || $this->name == 'must_be_magic' || $this->name == 'backline_defensive') {
+    } else if ($this->name == 'front_line_fire' || $this->name == 'formation_full_up' || $this->name == 'must_be_magic' || $this->name == 'backline_defensive' || $this->name == 'power_saws_ablaze') {
       $damage = bcmul(bcmul(bcpow(bcadd(bcdiv($this->damage_base_multiplier, 100, 40), 1, 40), $this->stacks, 40), 100, 40), bcadd(bcdiv($this->damage_base, 100, 40), 1, 40));
     } else if ($this->name == 'apprentice_crafter' || $this->name == 'journeyman_crafter' || $this->name == 'master_crafter') {
       $damage = bcmul(bcadd(bcmul(bcsub(bcpow(bcadd(bcdiv($this->damage_base_multiplier, 100, 40), 1, 40), $this->current_level, 40), 1, 40), $this->stacks, 40), 1, 40), 100, 40);
