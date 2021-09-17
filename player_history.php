224a0c78eec1fe495b99b1ba7e1392289ff99651
Grag Atrim <codingame@gragatrim.com>
2021-09-16 20:50:46 -0600

    Remove catch all
    There is a lot of entries that I haven't implemented yet and don't feel
    like, so just going to ignore them for now.... maybe one day I'll add
    them so they don't clog up the history page

--- player_history.php
+++ player_history.php
@@ -169,9 +169,10 @@ if (!empty($response)) {
           }
         } else if (!empty($entry->info->action) && ($entry->info->action == 'talent_payout')) {
             $player_history .= "<div><span style='font-weight: bold;'>" . $entry->history_date . "</span>: Gained  " . $entry->info->reward->amount . " common materials for " . $entry->info->reward->details . "</div>";
-        } else {
-          //Catch all in case I haven't implemented it yet
-          $player_history .= "<pre>" . print_r($entry, true) . "</pre>";
+        //Should eventually uncomment this and implement the things caught here... but I'm lazy
+        //} else {
+        //  //Catch all in case I haven't implemented it yet
+        //  $player_history .= "<pre>" . print_r($entry, true) . "</pre>";
         }
       } else if (!empty($entry->info->idols)) {
         $reset_reward = '';
